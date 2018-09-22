0991afd179b030d6c46872bbe629349388d8d570
Muhammad Walidin <mawalidin@gmail.com>
2018-09-22 18:46:01 +0700

    PBO3-10117094-Latihan1-Perintah Menampilkan Value Ke Layar

---
+++ src/pbo3/pkg10117094/latihan1/perintahmenampilkanvaluekelayar/PBO310117094Latihan1PerintahMenampilkanValueKeLayar.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117094.latihan1.perintahmenampilkanvaluekelayar;
+
+/**
+ *
+ * @author 
+ * NAMA                 : Muhammad Walidin 
+ * KELAS                : PBO-3 
+ * NIM                  : 10117094 
+ * Program Deskripsi    : Program ini berisi program untuk menampilkan biodata
+ */
+public class PBO310117094Latihan1PerintahMenampilkanValueKeLayar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Muhammad Walidin");
+        System.out.println("ALAMAT\t\t: Jalan Dipatiukur no 96");
+        System.out.println("TEMPAT LAHIR\t\t: Sukabumi");
+        System.out.println("TANGGAL LAHIR\t\t: 21 Agustus 1999");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Photografi & Renang");
+        System.out.println("MAKES\t\t: Mie");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+    
+}
